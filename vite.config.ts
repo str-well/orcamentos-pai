77f93b268c6d6bb0a1d0c9c194e2e0e4f968c192
Well <well@m23.studio>
2025-02-26 15:08:54 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
